92628ecc5fe9f600e15881831d76335bfec5b7a4
nahueldev23 <nahuel.dev.23@gmail.com>
2021-05-20 14:36:12 -0300

    botones filtro tipo

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,4 +1,5 @@
 <template>
+    <FilterByType/>
     <div v-for="(pokemon,index) in pokemons" :key="index">
         <div class="text-center">{{pokemon.name}}</div>
      <div v-for="item in pokemon.types" :key="item.id">
@@ -8,7 +9,9 @@
 </template>
 
 <script>
+import FilterByType from './FilterByType.vue'
 export default {
+  components: { FilterByType },
     name:'Card',
     props:['pokemons']
 }
---
+++ src/components/FilterByType.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="filterPokemons('grass')">grass</button>
+  <button @click="filterPokemons('poison')">poison</button>
+  <button @click="filterPokemons('fire')">fire</button>
+  <button @click="filterPokemons('flying')">flying</button>
+  <button @click="filterPokemons('water')">water</button>
+  <button @click="filterPokemons('bug')">bug</button>
+  <button @click="filterPokemons('normal')">normal</button>
+  <button @click="filterPokemons('electric')">electric</button>
+  <button @click="filterPokemons('ground')">ground</button>
+  <button @click="filterPokemons('fairy')">fairy</button>
+  <button @click="filterPokemons('psychic')">psychic</button>
+  <button @click="filterPokemons('fighting')">fighting</button>
+  <button @click="filterPokemons('rock')">rock</button>
+  <button @click="filterPokemons('steel')">steel</button>
+  <button @click="filterPokemons('ice')">ice</button>
+  <button @click="filterPokemons('')">all</button>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  methods: {
+    ...mapActions(["filterPokemons"]),
+  },
+};
+</script>
+
+<style>
+</style>
